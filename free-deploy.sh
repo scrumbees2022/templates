5d9a8f5523f0222fc7538c31c98861b3f06be338
Sarah Busch <sebusch@gmail.com>
2022-05-25 08:45:16 +1000

    add gh action to pythondeploy, and print docker webhook at end

--- free-deploy.sh
+++ free-deploy.sh
@@ -24,12 +24,14 @@ az appservice plan create --name $ASP --resource-group $RG --sku $SKU --is-linux
 az webapp create --resource-group $RG --plan $ASP \
 --name scrumbees-stroke-container \
 -i sebusch/stroke-prediction:latest
+
 # 3. Configure webapp to listen on correct port
 az webapp config appsettings set --resource-group $RG \
 --name scrumbees-stroke-container --settings WEBSITES_PORT=5000
 # 4. Turn on CD -- it doesn't actually make the webhook for you
 az webapp deployment container config --enable-cd true \
---name scrumbees-stroke-container --resource-group $RG --query CI_CD_URL --output tsv
+--name scrumbees-stroke-container --resource-group $RG \
+--query CI_CD_URL --output tsv > webhook_url.txt
 # This above command will output a URL which I need to paste into my docker repo
 
 
@@ -40,3 +42,11 @@ az deployment group create \
 --parameters appServicePlanPortalName=$ASP \
 --parameters sku=$SKU \
 --template-file "azuredeploy.json"
+
+# not perfect solution, but modity this deployment to add github actions.
+# required parameters: github repo, webapp name.
+az webapp deployment github-actions add --repo "scrumbees2022/strokeprediction" \
+-g $RG -n scrumbees-stroke-prediction --token $TOKEN
+
+echo "Webhook URL for Docker Hub repo:"
+cat webhook_url
