10d6af566561424600579801a46a698f7b47a42b
Sarah Busch <sebusch@gmail.com>
2022-05-25 12:47:55 +1000

    revert back to before changing template. configure deployment to add CI/CD

--- free-deploy.sh
+++ free-deploy.sh
@@ -43,10 +43,16 @@ az deployment group create \
 --parameters sku=$SKU \
 --template-file "azuredeploy.json"
 
+# Configure to CI/CD github actions
+# again not perfect way.
+az webapp deployment source config --name scrumbees-stroke-prediction \
+--resource-group $RG --repo-url "https://github.com/scrumbees2022/strokeprediction" \
+--branch main --git-token $TOKEN
+
 # not perfect solution, but modity this deployment to add github actions.
 # required parameters: github repo, webapp name.
-az webapp deployment github-actions add --repo "scrumbees2022/strokeprediction" \
--g $RG -n scrumbees-stroke-prediction --token $TOKEN
+# az webapp deployment github-actions add --repo "scrumbees2022/strokeprediction" \
+# -g $RG -n scrumbees-stroke-prediction --token $TOKEN
 
 echo "Webhook URL for Docker Hub repo:"
 cat webhook_url.txt
